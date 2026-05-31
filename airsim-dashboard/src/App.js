f235fd70a97bc76b06662ca757b769cc63ef1f4c
dbjin <dbjin38@gmail.com>
2026-05-31 23:23:00 +0900

    Test Push

--- airsim-dashboard/src/App.js
+++ airsim-dashboard/src/App.js
@@ -1,10 +1,27 @@
 import { useEffect, useState } from "react";
 
+const MOCK_DATA =
+    {
+  state: "RUNNING",
+  scenario: {
+    type: "RIGHT_TURN",
+    Pedestrian: true
+  },
+  ASIL: "B",
+  speed: 32,
+  collision: false
+};
+
 function App() {
 
-  const [state, setState] = useState({});
+  const [state, setState] = useState(MOCK_DATA);
 
   useEffect(() => {
+
+    // 개발 중에는 비활성화
+    const ENABLE_WEBSOCKET = false;
+
+    if (!ENABLE_WEBSOCKET) return;
 
     const ws = new WebSocket("ws://localhost:8000/ws");
 
@@ -13,7 +30,7 @@ function App() {
     };
 
     ws.onerror = (err) => {
-      console.log("WebSocket error", err);
+      console.log(err);
     };
 
     return () => ws.close();
@@ -26,13 +43,19 @@ function App() {
       <h1>🚗 AirSim Safety Dashboard</h1>
 
       <h3>STATE: {state.state}</h3>
-      <h3>SCENARIO: {JSON.stringify(state.scenario)}</h3>
-
-      <h3>ASIL LEVEL: {state.ASIL}</h3>
-      <h3>SPEED: {state.speed}</h3>
 
       <h3>
-        COLLISION: {state.collision ? "YES ❌" : "NO ✅"}
+        SCENARIO:
+        {JSON.stringify(state.scenario)}
+      </h3>
+
+      <h3>ASIL LEVEL: {state.ASIL}</h3>
+
+      <h3>SPEED: {state.speed} km/h</h3>
+
+      <h3>
+        COLLISION:
+        {state.collision ? " YES " : " NO "}
       </h3>
 
     </div>
